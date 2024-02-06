51db6f275b19689e85734eee3ec1164775ed953d
scpki <scpki@RILEYSLAPTOP>
2024-02-06 00:48:47 -0500

    Add project files.

---
+++ BetterTotalScrap/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace BetterTotalScrap
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class BetterTotalScrap : BaseUnityPlugin
+    {
+        private const string modGUID = "impulse.BetterTotalScrap";
+        private const string modName = "BetterTotalScrap";
+        private const string modVersion = "1.0.0";
+        private readonly Harmony harmony = new Harmony(modGUID);
+        void Awake()
+        {
+            harmony.PatchAll(typeof(HUDManagerPatch));
+        }
+    }
+}
---
+++ BetterTotalScrap/RoundManagerPatch.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using System.Linq;
+using UnityEngine;
+
+namespace BetterTotalScrap
+{
+    [HarmonyPatch(typeof(HUDManager), "FillEndGameStats")]
+    public class HUDManagerPatch
+    {
+        [HarmonyPostfix]
+        public static void FillEndGameStatsPostfix(HUDManager __instance, int scrapCollected)
+        {
+            int remainingScrapInLevel = CalculateRemainingScrapInLevel();
+            float finalCount = (int)(scrapCollected + remainingScrapInLevel);
+
+            finalCount += 80 * GameObject.FindObjectsOfType<LungProp>().Where(lung => lung.isLungDocked).Count();
+
+            __instance.statsUIElements.quotaDenominator.text = finalCount.ToString();
+        }
+        private static int CalculateRemainingScrapInLevel()
+        {
+            GrabbableObject[] array = Object.FindObjectsOfType<GrabbableObject>();
+            int remainingValue = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].itemProperties.isScrap && !array[i].isInShipRoom && !array[i].isInElevator && !array[i].scrapPersistedThroughRounds)
+                {
+                    remainingValue += array[i].scrapValue;
+                }
+            }
+            return remainingValue;
+        }
+    }
+}
